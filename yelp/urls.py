ff3190876232144109c90426a052c54646efae19
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-05-26 18:32:56 +0300

    initial commit

---
+++ yelp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+                    
+urlpatterns = [
+    path('', views.index),
+    path('process', views.create),
+    path('process2', views.login),
+    path('login', views.log),
+    path('category/<int:id>', views.show)
+]
